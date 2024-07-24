6fec767a1b524a874287ca699d0cff6a62a00315
Evan Wang <exwang@uwaterloo.ca>
2024-07-24 19:52:56 -0400

    Rating Fix

--- backend/models/user.go
+++ backend/models/user.go
@@ -9,9 +9,9 @@ type User struct {
 	Email          string    `gorm:"uniqueIndex;not null"`
 	Username       string    `gorm:"uniqueIndex;not null"`
 	PasswordHash   string    `gorm:"not null"`
-	ProfilePicture string    `gorm:"not null"`
 	FirstName      string    `gorm:"not null"`
 	LastName       string    `gorm:"not null"`
+	ProfilePicture string    `gorm:"not null"`
 	DateCreated    time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
 }
 
@@ -22,31 +22,34 @@ type Follow struct {
 
 type Location struct {
 	LocationID uint    `gorm:"primaryKey;autoIncrement"`
-	Country    string  `gorm:"not null"`
-	City       string  `gorm:"not null"`
+	Country    string  `gorm:"uniqueIndex:idx_country_city"`
+	City       string  `gorm:"uniqueIndex:idx_country_city"`
 	Latitude   float64 `gorm:"not null"`
 	Longitude  float64 `gorm:"not null"`
 }
 
 type Post struct {
-	PostID        uint   `gorm:"primaryKey;autoIncrement"`
-	UserID        uint   `gorm:"not null"`
-	Description   string `gorm:"not null"`
-	CommentsCount int    `gorm:"not null"`
-	Image         string `gorm:"not null"`
-	LocationID    uint   `gorm:"not null"`
+	PostID        uint      `gorm:"primaryKey;autoIncrement"`
+	UserID        uint      `gorm:"not null"`
+	Description   string    `gorm:"not null"`
+	CommentsCount int       `gorm:"not null"`
+	Image         string    `gorm:"not null"`
+	LocationID    uint      `gorm:"default:null"`
+	DateCreated   time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
 }
 
 type Comment struct {
-	CommentID uint   `gorm:"primaryKey;autoIncrement"`
-	PostID    uint   `gorm:"not null"`
-	UserID    uint   `gorm:"not null"`
-	Content   string `gorm:"not null"`
+	CommentID   uint      `gorm:"primaryKey;autoIncrement"`
+	PostID      uint      `gorm:"not null"`
+	UserID      uint      `gorm:"not null"`
+	Content     string    `gorm:"not null"`
+	DateCreated time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
 }
 
 type Rating struct {
-	UserID     uint `gorm:"primaryKey"`
-	LocationID uint `gorm:"primaryKey"`
-	Rating     int  `gorm:"not null"`
+	UserID      uint      `gorm:"primaryKey"`
+	LocationID  uint      `gorm:"primaryKey"`
+	Rating      int       `gorm:"not null"`
+	Review      string    `gorm:"default:null"`
+	DateCreated time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
 }
-
